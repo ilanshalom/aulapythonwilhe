b180b0d42bdb56a120b73e58d96460a411beff62
DAVID <ilanelvaron24@gmail.com>
2024-04-09 10:52:09 -0300

    jogo atualizado

---
+++ codigo.py
@@ -0,0 +1,15 @@
+from random import *
+num = randint(0,10)
+i = 0
+controle = 0
+
+while controle ==0: 
+    i=i+1
+    x = int(input("Digite um numero inteiro:"))
+    if num == x:
+     print("Parabens voce acertou em %d tentativas" %i)
+     controle = 1
+    elif num > x:
+        print("O numero pensado é maior!")
+    else:
+        print("o numero pensado é menor")
